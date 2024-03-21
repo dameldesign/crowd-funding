dfe42c962315742edad559e5b396da6dd7f4a90d
dameldesign <dameldesign23@gmail.com>
2024-03-21 06:29:44 +0100

    commit

---
+++ lib/validations.ts
@@ -0,0 +1,40 @@
+import * as z from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(6)
+    .refine(
+      (pass) => {
+        return /[0-9]/.test(pass) && /[A-Z]/.test(pass);
+      },
+      {
+        message:
+          "Password must be at least 6 characters long and contain at least one number and one uppercase letter.",
+      }
+    ),
+  remember: z.boolean().default(false).optional(),
+});
+
+export const SignupSchema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  mobile: z.string(),
+  password: z
+    .string()
+    .min(6)
+    .refine(
+      (pass) => {
+        return /[0-9]/.test(pass) && /[A-Z]/.test(pass);
+      },
+      {
+        message:
+          "Password must be at least 6 characters long and contain at least one number and one uppercase letter.",
+      }
+    ),
+});
+
+export const ForgotPasswordSchema = z.object({
+  email: z.string().email(),
+});
